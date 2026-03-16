1c9d85af4d32bb754ecc07d039a0f8a57dc03e8d
malboushi <mohammad.alaa.love.619@gmail.com>
2026-03-16 04:14:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vault-cache-v7';
+const CACHE_NAME = 'vault-cache-v4';
 
 const urlsToCache = [
   './',
@@ -10,8 +10,27 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // إجبار المتصفح على تفعيل التحديث فوراً
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// السر هنا: هذه الدالة تقوم بمسح أي كاش قديم عند تحديث رقم الإصدار
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('تم حذف الكاش القديم:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
